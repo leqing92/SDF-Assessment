3f9a21ce4792d1050f2e39e1d9a60e43aa249ead
leqing92 <leqing92@hotmail.com>
2024-03-01 16:51:12 +0800

    first commit

--- src/App.java
+++ src/App.java
@@ -134,7 +134,7 @@ public class App {
                 int enteredPokemonIndex = eachStack.indexOf(enteredPokemon);
                 int cardsToGo = -1;
                 for(int j = 0; j < eachStack.size(); j++){
-                    if(eachStack.get(j).startsWith("5*") && !eachStack.get(j).equals(enteredPokemon)){
+                    if(eachStack.get(j).startsWith("5*") && j != enteredPokemonIndex){
                         cardsToGo = j;
                         break;                        
                     }
